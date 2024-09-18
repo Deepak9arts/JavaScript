22d76b6f1936196bd4922f7bbda2fd5cbf80fba4
Deepak9arts <coc9artsdragon.dk@gmail.com>
2024-09-19 02:36:28 +0530

    Variables Done

---
+++ Basics/Variables.js
@@ -0,0 +1,12 @@
+const accountID= 1234567890;;
+
+let emai="Deepak@gmail.com"
+
+var accountPasword="Deep12"
+
+accountCity="Bangalore"
+
+let accountState 
+
+
+console.table([accountID,emai,accountPasword,accountCity,accountState]);
